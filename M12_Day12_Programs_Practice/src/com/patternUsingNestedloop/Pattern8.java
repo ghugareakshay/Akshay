0e1ed74ae2eab7264a327c259afc321516af8706
Admin <Admin@DESKTOP-DCJAPEJ>
2022-03-21 03:48:57 -0700

    only defination

---
+++ M12_Day12_Programs_Practice/src/com/patternUsingNestedloop/Pattern8.java
@@ -0,0 +1,20 @@
+package com.patternUsingNestedloop;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=3;i++) {
+			for(int j=3;j>=i;j--) {
+				System.out.print("");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.println("*");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ M12_Day14_wrapper_calss_autoboxing/src/Defination.java
@@ -0,0 +1,17 @@
+
+public class Defination {
+
+	public static void main(String[] args) {
+	
+       System.out.println("Wrapper class:");
+       System.out.println("The wrapper class in Java provides the mechanism to convert primitive into object and object into primitive.");
+       System.out.println("Example:");
+       System.out.println("boolean To	Boolean");
+       
+       
+       System.out.println("\n\nautoboxing:");
+       System.out.println("The automatic conversion of primitive data type into its corresponding wrapper class is known as autoboxing");
+       System.out.println("Example:\nbyte To Byte");
+	}
+
+}
